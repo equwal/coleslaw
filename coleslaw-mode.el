5ef13ba3065d7d279236374e5c5dd3cdb10e322a
Spenser Truex <spensertruexonline@gmail.com>
2018-12-09 18:41:17 -0800

    Version 1.1.0 for MELPA and ELPA release.

--- coleslaw-mode.el
+++ coleslaw-mode.el
@@ -1,77 +1,71 @@
-;;; coleslaw-mode.el --- Mode for HTML static content generation with Coleslaw
+-*- lexical-binding: t; -*-
+;;; coleslaw-mode.el --- Coleslaw static content files.
 
 ;; Copyright (C) 2018 Spenser Truex
 ;; Author: Spenser Truex <spensertruexonline@example.com>
 ;; Created: Equwal 2018-12-09
-;; Version: 1.0
-;; Keywords: Coleslaw Static Content Mode with Skeleton Comment
+;; Version:  1.1.0
+;; Keywords: lisp wp files convenience
 ;; URL: https://github.com/equwal/coleslaw-mode/
 ;; Homepage: http://truex.eu
 ;; This file is not part of GNU Emacs.
-;; This file is free software licensed with the GNU GPL v3 see
+;; This file is free software.
+;; License:
+;; Licensed with the GNU GPL v3 see:
 ;; <https://www.gnu.org/licenses/>.
-;;; Commentary: `coleslaw-insert-header` (bound to `M-;`) inserts the
-;;; comment block depending on the type of file, and is how the major mode
-;;; is selected for the file. Meant to work for all formats supported by
-;;; kingcons's "coleslaw" static content generator.
+
+;;; Commentary:
+;; coleslaw-insert-header (bound to M-;) inserts the comment block
+;; depending on the type of file, and is how the major mode is selected for
+;; the file.  Meant to work for all formats supported by kingcons's
+;; "coleslaw" static content generator. Default enabled in .page and .post
+;; files.
 
 ;;; Code:
 
-(defun group (ps n)
-  (if (zerop n) (error "zero length"))
-  (labels ((rec (source acc)
-                (let ((rest (nthcdr n source)))
-                  (if (consp rest)
-                      (rec rest (cons
-                                 (subseq source 0 n)
-                                 acc))
-                    (nreverse
-                     (cons source acc))))))
-    (if ps (rec ps nil) nil)))
 (defvar coleslaw-mode-hook nil)
-(defun bufftype (type)
+(defun coleslaw-bufftype (type)
+  "Determine if the file type of the current buffer is TYPE."
   (string-equal type (subseq buffer-file-name (- (length buffer-file-name) 5))))
+;;;###autoload
 (defun coleslaw-insert-header (format)
+  "Spawn a skeleton as specified by default for a coleslaw file type.
+Automatically changes the mode.  FORMAT is filled into the
+skeleton and used to select the mode"
   (interactive "Sformat: ")
   (beginning-of-buffer)
   (if (bufftype ".post")
-      (progn (insert (concatenate 'string
-                                  ";;;;;
-title: 
-format: "
-                                  format
-                                  "
-date: 
-;;;;;
-<!--more-->
-
-<!--more-->
-"))
+      (progn (insert (concatenate
+                      'string
+                      ";;;;;\title: \format: "
+                      format
+                      "\date: \;;;;;\<!--more-->\\<!--more-->\"))
              (beginning-of-buffer)
-             (next-line)
+             (forward-line)
              (move-end-of-line 1))
-    (if (bufftype ".page")
-        (progn (insert (concatenate 'string
-                                    ";;;;;
-title: 
-url: 
-format: "
-                                    format
-                                    "
-date: 
-;;;;;"))
-               (beginning-of-buffer)
-	       (next-line)
-	       (move-end-of-line)
-               (cond ((string-equal (symbol-name format) "md") (markdown-mode))
-                     ((string-equal (symbol-name format) "cl-who") (lisp-mode))
-                     ((string-equal (symbol-name format) "html") (html-mode))
-                     ((string-equal (symbol-name format) "rst") (markdown-mode)))))))
+    (when (bufftype ".page")
+      (insert (concatenate
+               'string
+               ";;;;;\title: \url: \format: "
+               format
+               "\date: \;;;;;"))
+      (beginning-of-buffer)
+      (forward-line)
+      (move-end-of-line)
+      (cond ((string-equal (symbol-name format) "md")
+             (markdown-mode))
+            ((string-equal (symbol-name format) "cl-who")
+             (lisp-mode))
+            ((string-equal (symbol-name format) "html")
+             (html-mode))
+            ((string-equal (symbol-name format) "rst")
+             (markdown-mode))))))
 (defvar coleslaw-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "M-;") 'coleslaw-insert-header)
     map)
-  "Keymap for COLESLAW major mode")
+  "Keymap for COLESLAW major mode.")
+;;;###autoload
 (defun coleslaw-mode ()
   "Mode for editing coleslaw site generation files."
   (interactive)
@@ -92,4 +86,6 @@ date:
   "Major mode for editing Markdown files")
 (autoload 'markdown-preview-eww "view markdown in w3m web browser.")
 
-;;; coleslaw.el ends here
+(provide 'coleslaw-mode)
+
+;;; coleslaw-mode.el ends here
